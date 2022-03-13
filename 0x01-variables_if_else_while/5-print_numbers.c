85d5f71a722bb9c9764c6f1ed2a8a856aae10552
joyngumo <joyngumo@gmail.com>
2022-03-13 18:47:46 +0300

    Base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+/**
+ * main - Main program
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+		printf("%d", x);
+
+	printf("\n");
+
+	return (0);
+}
