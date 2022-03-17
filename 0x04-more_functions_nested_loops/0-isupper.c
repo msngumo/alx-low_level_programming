67e4abad9dbaf218ddf2884ce71282bb608b4918
joyngumo <joyngumo@gmail.com>
2022-03-18 01:20:05 +0300

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 c == uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c  >= 65 && c <= 90);
+}
