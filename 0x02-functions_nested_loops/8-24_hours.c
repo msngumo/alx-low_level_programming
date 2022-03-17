bb6be2419a91b246a747728770e501d16746bc57
joyngumo <joyngumo@gmail.com>
2022-03-17 05:32:26 +0300

    minute of jack.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,12 +6,12 @@
 
 void jack_bauer(void)
 {
-	int hrs = '0';
-	int min = '0';
+	int hrs;
+	int min;
 
-	for (; hrs <= 23; hrs++)
+	for (hrs = 0; hrs <= 23; hrs++)
 	{
-		for (; min <= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar(hrs / 10 + '0');
 			_putchar(hrs % 10 + '0');
