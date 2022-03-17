4ecadad32534fa976801aaf77db05e460b4c3417
joyngumo <joyngumo@gmail.com>
2022-03-18 02:27:07 +0300

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers -  print numbers from 0 - 9
+ *
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+		_putchar(x);
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
  function that checks for uppercase character
  function that checks for a digit (0 through 9).
+ prints the numbers, from 0 to 9, followed by a new line.
