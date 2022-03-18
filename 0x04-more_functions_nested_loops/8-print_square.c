6916bd7cb16c1ab77fb822e4b7dbafad93c07858
joyngumo <joyngumo@gmail.com>
2022-03-18 05:24:33 +0300

    draw square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	if (size >= 1)
+	{
+		int i = 1;
+
+		for (; i <= size; i++)
+		{
+			int j = 1;
+
+			for (; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
