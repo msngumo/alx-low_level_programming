29865ab9b23821a4d81c53764ab1adc407c8afe3
joyngumo <joyngumo@gmail.com>
2022-03-22 02:26:22 +0300

    half of string.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -23,9 +23,9 @@ void puts_half(char *str)
 	}
 	else
 	{
-		int iter = n -1;
+		int iter = n - 1;
 
-		while (iter <= _strlen(str) - 1)
+		while (iter <= _strlen(str))
 		{
 			_putchar(str[iter]);
 			iter++;
